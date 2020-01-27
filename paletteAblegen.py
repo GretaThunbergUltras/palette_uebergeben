e2f739b5d501f4001bd192745e92c6504ec4bc5e
Pi <pi@pi.pi>
2020-01-27 14:50:14 +0000

    prototype for turning around

--- paletteAblegen.py
+++ paletteAblegen.py
@@ -6,6 +6,15 @@
 
 '''
 
+from botlib.bot import Bot
+import time
+
+bot = None
+    
+def initialisiereRoboter():
+    global bot
+    bot = Bot()
+    bot.calibrate()
 
 def scanNachEntgegenkommendemFahrzeug():
     #TODO realisieren der kompletten Funktion. Eventuell aufnehmen in die Botlib
@@ -25,10 +34,49 @@ def paletteAbsetzen():
 
 def umdrehen():
     #TODO realisieren der kompletten Funktion, oder einbinden von der Botlib
-    pass
+    '''
+bp.set_motor_position(bp.PORT_D, 70)
+time.sleep(1)
+bp.set_motor_power(bp.PORT_B, -30)
+time.sleep(4)
+bp.set_motor_power(bp.PORT_B, 0)
+time.sleep(1)
+bp.set_motor_position(bp.PORT_D, 0)
+time.sleep(1)
+bp.set_motor_position(bp.PORT_D, -60)
+bp.set_motor_power(bp.PORT_B, 30)
+time.sleep(4)
+bp.set_motor_power(bp.PORT_B, 0)
+bp.set_motor_position(bp.PORT_D, 0)
+time.sleep(1)
+bp.set_motor_power(bp.PORT_B, 20)
+time.sleep(1)
+bp.set_motor_power(bp.PORT_B, 0)'''
+    bot.drive_steer(1)
+    time.sleep(1)
+    bot.drive_power(-30)
+    time.sleep(3.52)
+    bot.drive_power(0)
+    time.sleep(1)
+    bot.drive_steer(0)
+    time.sleep(1)
+    bot.drive_steer(-1)
+    bot.drive_power(30)
+    time.sleep(3.52)
+    bot.drive_power(0)
+    bot.drive_steer(0)
+    time.sleep(1)
+    bot.drive_power(20)
+    time.sleep(1)
+    bot.drive_power(0)
 
 def nachrichtAnFahrzeugZwei():
     #TODO realisieren der kompletten Funktion
     #TODO Gedanken über implementation mit MQTT machen
     #Nachfrage möglich bei: Mercedes, Kris
     pass
+
+def test():
+    initialisiereRoboter()
+    umdrehen()
+test()
